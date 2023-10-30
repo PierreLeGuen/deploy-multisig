8d0287386c64bf74648aeac5851734e87632e30a
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-10-30 13:04:14 +0100

    v0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { WalletSelectorContextProvider } from "@/contexts/WalletSelectorContext";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import "@near-wallet-selector/modal-ui/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletSelectorContextProvider>
+      <Component {...pageProps} />
+    </WalletSelectorContextProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,160 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useWalletSelector } from "@/contexts/WalletSelectorContext";
+import { parseNearAmount } from "near-api-js/lib/utils/format";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 
-const inter = Inter({ subsets: ['latin'] })
+type MultisigForm = {
+  accountId: string;
+  threshold: number;
+  signers: string[];
+};
 
 export default function Home() {
+  const { selector, modal, accounts, accountId } = useWalletSelector();
+  const {
+    control,
+    handleSubmit,
+    register,
+    setError,
+    formState: { errors },
+    watch,
+  } = useForm<MultisigForm>();
+
+  // State for current network
+  const [currentNetwork, setCurrentNetwork] = useState("testnet");
+
+  const handleSignIn = () => {
+    modal.show();
+  };
+
+  const handleSignOut = async () => {
+    if (!accountId) return;
+    const w = await selector.wallet();
+    await w.signOut();
+  };
+
+  const onSwitchNetwork = async (network: string) => {
+    await handleSignOut();
+    setCurrentNetwork(network);
+  };
+
+  const onSubmit = (data: MultisigForm) => {
+    if (data.signers.length < data.threshold) {
+      setError("signers", {
+        type: "manual",
+        message:
+          "Number of signers must be equal or greater than the threshold.",
+      });
+      return;
+    }
+    createMultisigWithFactory(data);
+  };
+
+  const createMultisigWithFactory = async (data: MultisigForm) => {
+    const factory =
+      currentNetwork === "testnet"
+        ? "multisignature.testnet"
+        : "multisignature.near";
+    const wallet = await selector.wallet();
+
+    await wallet.signAndSendTransactions({
+      transactions: [
+        {
+          receiverId: factory,
+          actions: [
+            {
+              type: "FunctionCall",
+              params: {
+                methodName: "create",
+                args: {
+                  name: data.accountId,
+                  members: data.signers,
+                  num_confirmations: data.threshold,
+                },
+                gas: "300000000000000",
+                deposit: parseNearAmount("5")!,
+              },
+            },
+          ],
+        },
+      ],
+    });
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col p-10">
+      <div className="flex justify-end">
+        <div className="">
+          <div
+            className={`flex ${!accountId ? "" : "hidden"}`}
+            onClick={handleSignIn}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Sign In
+          </div>
+          <div
+            className={`flex ${!accountId ? "hidden" : ""}`}
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </div>
+          {/* Network Toggle */}
+          <div className="mt-2 flex flex-col">
+            Current Network: {currentNetwork}
+          </div>
+          <div className="flex flex-col gap-1">
+            Switch Network:
+            <button onClick={() => onSwitchNetwork("testnet")}>Testnet</button>
+            <button onClick={() => onSwitchNetwork("mainnet")}>Mainnet</button>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex justify-center w-full px-10">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full gap-3 flex flex-col"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div>
+            <label htmlFor="accountId">Account ID:</label>
+            <input id="accountId" {...register("accountId")} required />
+          </div>
+          <div>
+            <label htmlFor="threshold">Threshold:</label>
+            <input
+              id="threshold"
+              type="number"
+              {...register("threshold")}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="signers">Signers:</label>
+            <Controller
+              name="signers"
+              control={control}
+              defaultValue={[]}
+              rules={{
+                validate: (value) =>
+                  (value && value.length >= (watch("threshold") || 0)) ||
+                  "Number of signers must be equal or greater than the threshold.",
+              }}
+              render={({ field }) => (
+                <textarea
+                  {...field}
+                  placeholder="Enter signers separated by comma or new line"
+                  onChange={(e) =>
+                    field.onChange(e.target.value.split(/,\s*|\n+/))
+                  }
+                />
+              )}
+            />
+            {errors.signers && <span>{errors.signers.message}</span>}
+          </div>
+          <div>
+            <button type="submit">Create Multisig Wallet</button>
+          </div>
+        </form>
       </div>
     </main>
-  )
+  );
 }
